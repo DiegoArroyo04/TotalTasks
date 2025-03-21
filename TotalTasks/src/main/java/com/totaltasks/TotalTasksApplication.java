6092e1599af5ce64ed1fad7b757e607bdffad847
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-03-21 21:03:03 +0100

    Estructuración Proyecto

---
+++ TotalTasks/src/main/java/com/totaltasks/TotalTasksApplication.java
@@ -0,0 +1,13 @@
+package com.totaltasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalTasksApplication.class, args);
+	}
+
+}
