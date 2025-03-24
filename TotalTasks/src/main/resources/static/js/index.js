fbb084bd4ae7ba4e8ac449ce2cb8c90bf9bc4983
JrubioCode <javii.central.7@gmail.com>
2025-03-25 00:55:53 +0100

    Integraciones y reseñas en Lobby

--- TotalTasks/src/main/resources/static/js/index.js
+++ TotalTasks/src/main/resources/static/js/index.js
@@ -1,20 +1,18 @@
-// Caracterteristicas
+// Funcionalidades
 document.addEventListener("DOMContentLoaded", () => {
-  const botonesPestana = document.querySelectorAll(".boton-pestana");
-  const contenidosPestana = document.querySelectorAll(".contenido-pestana");
+  const botonesPestana = document.querySelectorAll(".pestanas-funcionalidades .boton-pestana");
+  const contenidosPestana = document.querySelectorAll(".pestanas-funcionalidades .contenido-pestana");
 
   botonesPestana.forEach(boton => {
     boton.addEventListener("click", () => {
-
-      // Elimina la clase "active" de todos los botones y contenidos
+      // Remueve la clase "active" de todos los botones y contenidos
       botonesPestana.forEach(b => b.classList.remove("active"));
       contenidosPestana.forEach(c => c.classList.remove("active"));
 
-      // Agrega la clase "active" al botón clicado y muestra su contenido
+      // Activa el botón clicado y muestra su contenido
       boton.classList.add("active");
       const idPestana = boton.getAttribute("data-pestana");
       document.getElementById(idPestana).classList.add("active");
-      
     });
   });
 });
