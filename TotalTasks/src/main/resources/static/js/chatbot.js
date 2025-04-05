8cda552abd6d43d5d5b855333d379d777dd25d95
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-04-06 01:27:55 +0200

    Chatbot Integrado

---
+++ TotalTasks/src/main/resources/static/js/chatbot.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    //TIEMPO DE CARGA DEL BOTON
+    setTimeout(() => {
+        document.getElementById("chatToggle").classList.add("visible");
+    }, 400);
+
+
+
+
+    document.getElementById("chatToggle").addEventListener("click", function () {
+        const chatbot = document.getElementById("chatWindow");
+
+        if (chatbot.style.display === "none" || chatbot.style.display === "") {
+            openChatbot();
+        } else {
+            chatbot.style.display = "none";
+        }
+    });
+
+
+
+
+    function openChatbot() {
+        document.getElementById("chatWindow").style.display = "flex";
+    }
+
+
+
+});
+
+
+function handleOptionClick(opcion) {
+    const chatLog = document.getElementById("chatLog");
+    const respuestas = {
+        "crear tarea recurrente": "Para crear una tarea recurrente, selecciona la opción 'Repetir' al momento de agregar una nueva tarea.",
+        "sincronizar con calendario": "Actualmente, ofrecemos inicio de sesión con Google, lo que facilitará la sincronización con Google Calendar próximamente",
+        "seguridad de los datos": "Utilizamos protocolos de seguridad y cifrado de última generación para proteger tu información en todo momento.",
+
+    };
+    const respuesta = respuestas[opcion]
+
+    // Mostrar la opción elegida
+    chatLog.innerHTML += `<div class="chat user">👤 ${opcion}</div>`;
+
+    // Mostrar respuesta del bot
+    chatLog.innerHTML += `<div class="chat bot">🤖 ${respuesta}</div>`;
+
+    // Hacer scroll automático
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
