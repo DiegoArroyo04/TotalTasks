a0268e5e0af55d69fcacd94fae71a3c052f6a181
JrubioCode <javii.central.7@gmail.com>
2025-04-10 18:51:22 +0200

    Ajuste de suavidad de header fijo

--- TotalTasks/src/main/resources/static/js/fragmentos/header.js
+++ TotalTasks/src/main/resources/static/js/fragmentos/header.js
@@ -1,13 +1,13 @@
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     const header = document.getElementById("cabezera");
     const spacer = document.getElementById("headerSpacer");
     const headerHeight = header.offsetHeight;
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 120) {
         header.classList.add("fixed");
         spacer.style.height = `${headerHeight}px`;
     } else {
         header.classList.remove("fixed");
-        spacer.style.height = `0px`;
+        spacer.style.height = '0px';
     }
 });
