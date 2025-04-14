104d346be19c4205482550f3b583dab7ca6467bc
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-04-14 20:34:45 +0200

    Cargar proyectos en dashboard

---
+++ TotalTasks/src/main/java/com/totaltasks/services/ProyectoService.java
@@ -0,0 +1,10 @@
+package com.totaltasks.services;
+
+import java.util.List;
+
+import com.totaltasks.entities.UsuarioEntity;
+import com.totaltasks.entities.ProyectoEntity;
+
+public interface ProyectoService {
+    List<ProyectoEntity> todosLosProyectosDeUnUsuario(UsuarioEntity usuario);
+}
---
+++ TotalTasks/src/main/java/com/totaltasks/services/implementations/ProyectoServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.totaltasks.services.implementations;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.totaltasks.entities.ProyectoEntity;
+import com.totaltasks.entities.UsuarioEntity;
+import com.totaltasks.repositories.ProyectoRepository;
+import com.totaltasks.services.ProyectoService;
+
+@Service
+public class ProyectoServiceImplementation implements ProyectoService {
+
+    @Autowired
+    private ProyectoRepository proyectoRepository;
+
+    @Override
+    public List<ProyectoEntity> todosLosProyectosDeUnUsuario(UsuarioEntity usuario) {
+        return proyectoRepository.findTodosLosProyectosDeUnUsuario(usuario.getIdUsuario());
+    }
+
+}
